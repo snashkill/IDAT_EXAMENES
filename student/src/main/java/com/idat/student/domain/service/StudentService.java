de79dc9f7a88a74fb86ed9af49622268cd50f16c
snashkill <renatosotocoral@outlook.com>
2025-06-03 03:15:11 -0500

    StudentService.java

--- student/src/main/java/com/idat/student/domain/service/StudentService.java
+++ student/src/main/java/com/idat/student/domain/service/StudentService.java
@@ -4,10 +4,12 @@ import com.idat.student.domain.model.Student;
 
 import java.util.List;
 
+//SERVICE MODIFICADO
 public interface StudentService {
     List<Student> findAll();
     Student findById(String uid);
     Student save(Student student);
     Student update(Student student);
+    Student update(String uid, Student student);
     void delete(String uid);
 }
