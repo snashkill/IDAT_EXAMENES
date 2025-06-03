cdf9a2a8f1e5375ad7705a913fcf97c3fe345ead
snashkill <renatosotocoral@outlook.com>
2025-06-03 03:15:49 -0500

    Student.java

--- student/src/main/java/com/idat/student/domain/model/Student.java
+++ student/src/main/java/com/idat/student/domain/model/Student.java
@@ -1,87 +1,13 @@
 package com.idat.student.domain.model;
 
-//CLASE STUDENT MODIFICADA
-public class Student {
-
-    private String uid;
-    private String documentNumber;
-    private String name;
-    private String lastName;
-    private String phone;
-    private String email;
-    private String photo;
-    private int active;
-
-    public Student() {}
-
-    // Getters y Setters
-
-    public String getUid() {
-        return uid;
-    }
-
-    public void setUid(String uid) {
-        this.uid = uid;
-    }
-
-    public String getDocumentNumber() {
-        return documentNumber;
-    }
-
-    public void setDocumentNumber(String documentNumber) {
-        this.documentNumber = documentNumber;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoto() {
-        return photo;
-    }
-
-    public void setPhoto(String photo) {
-        this.photo = photo;
-    }
-
-    public int getActive() {
-        return active;
-    }
-
-    public void setActive(int active) {
-        this.active = active;
-    }
-
-    public void Setuid(String uid) {
-    }
-
-
+public record Student(
+        String uid,
+        String documentNumber,
+        String name,
+        String lastName,
+        String phone,
+        String email,
+        String photo,
+        int active
+) {
 }
